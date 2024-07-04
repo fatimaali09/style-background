5f01eda5b4b588e4b26c2c15a4016fb0982a5db0
fatimaalisoy <fatimaalisoy09@gmail.com>
2024-07-04 15:26:09 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+console.log(7)
+
+// data type
+// number - integer
+// string
+
+
+
+document.getElementById('pink_button').addEventListener('click', ()=>{
+         console.log('click etdim');
+document.getElementById('container').style.backgroundColor='pink';})
+
+
+document.getElementById('yellow_button').addEventListener('click', ()=>{
+    console.log('click etdim');
+document.getElementById('container').style.backgroundColor='yellow';})
+
+document.getElementById('blue_button').addEventListener('click', ()=>{
+    console.log('click etdim');
+document.getElementById('container').style.backgroundColor='blue';})
+
+
+
+
+
+
+
+
